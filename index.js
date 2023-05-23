dc7ea72f45387d21768ac88377a55712bdfe4b96
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-05-23 21:57:50 +0200

    Flow field with points

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const lineObjects = [
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  background(0);
   const divisions = 20;
 
   for (let i = 0; i < divisions + 2; i++) {
@@ -27,7 +28,8 @@ function setup() {
           40 + random(0, 50),
           166 + random(-30, 30)
         ),
-        velocity: { x: 0, y: 0 },
+        dir: { x: 0, y: 0 },
+        speed: random(0.5, 2),
       };
 
       lineObjects.push(newLineObject);
@@ -38,25 +40,48 @@ function setup() {
   console.log(lineObjects);
 }
 
+const maxLength = 30;
+const noiseScale = 500;
+const noiseStrength = 1;
+
+function movePoint(point, dir, speed) {
+  const noiseFactor = noise(
+    point.x / noiseScale,
+    point.y / noiseScale,
+    frameCount / noiseScale
+  );
+  let angle = noiseFactor * TWO_PI * noiseStrength;
+  dir.x = cos(angle);
+  dir.y = sin(angle);
+  const vel = { x: dir.x, y: dir.y };
+  const d = 1;
+  vel.x = vel.x * speed * d;
+  vel.y = vel.y * speed * d;
+
+  point.x += vel.x;
+  point.y += vel.y;
+
+  if (point.x < 0 || point.x > width || point.y < 0 || point.y > height) {
+    point.x = random(width * 1.2);
+    point.y = random(height);
+  }
+}
+
 function draw() {
-  background(color(16, 2, 26));
+  noStroke();
+  fill(0, 10);
+  rect(0, 0, width, height);
+
   lineObjects.forEach((lineObject) => {
-    drawLine(lineObject.point1, lineObject.point2, lineObject.color);
-    lineObject.point2.x += lineObject.velocity.x;
-    lineObject.point2.y += lineObject.velocity.y;
-
-    lineObject.velocity.x += random(-0.1, 0.1);
-    lineObject.velocity.y += random(-0.1, 0.1);
-    // lineObject.point2.x = constrain(lineObject.point2.x, -10, 10);
-    // lineObject.point2.y = constrain(lineObject.point2.y, -10, 10);
-
-    lineObject.point2 = { x: mouseX, y: mouseY };
+    movePoint(lineObject.point2, lineObject.dir, lineObject.speed);
+    fill(lineObject.color);
+    circle(lineObject.point2.x, lineObject.point2.y, 10);
   });
 }
 
 function drawLine(point1, point2, color) {
   stroke(color);
-  strokeWeight(4);
+  strokeWeight(2);
   line(point1.x, point1.y, point2.x, point2.y);
 }
 
